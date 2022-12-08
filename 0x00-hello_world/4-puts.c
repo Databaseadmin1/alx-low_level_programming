60cd673c705eda649d9d220212f1a8d824cf0dc7
Databaseadmin1 <joniasmarcos0@gmail.com>
2022-12-08 12:59:16 -0800

    bettando

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-
+/**
+ *main - Prints "\"Programming is like building a multilingual puzzle"
+ *
+ * Return: zero
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
