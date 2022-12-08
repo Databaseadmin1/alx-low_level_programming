3418d3f2de4798142eecbc1de96122f5d882dacd
Databaseadmin1 <joniasmarcos0@gmail.com>
2022-12-08 15:45:55 -0800

    2prhass

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, ");
+	printf("  but the outcome is a piece of art,");
 	return (0);
 }
