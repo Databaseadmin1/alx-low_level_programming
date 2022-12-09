b9969d999699680a8632cc6db71f154070603e4f
Databaseadmin1 <joniasmarcos0@gmail.com>
2022-12-09 01:07:24 -0800

    last line switch with the above line

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,7 @@ int main(void)
 {
 	printf("Size of a char:%lubytes\n", sizeof(char));
 	printf("Size of an int:%lubytes\n", sizeof(int));
-	printf("Size of a long int:%lubytes\n", sizeof(long int));
-	printf("size of a float: %lu bytes\n", sizeof(float));
-	printf("Size of a long long int:%lubytes\n", sizeof(long long int));
+	printf("Size of a long int:%lubytes\n", sizeof(long));
+	printf("Size of a float%lubytes\n", sizeof(float));
 	return (0);
 }
