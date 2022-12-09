9cd0196ce772a596d5b2368b66615f943e1584b7
Databaseadmin1 <joniasmarcos0@gmail.com>
2022-12-09 01:14:51 -0800

    correct output

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of a char:%lubytes\n", sizeof(char));
-	printf("Size of an int:%lubytes\n", sizeof(int));
-	printf("Size of a long int:%lubytes\n", sizeof(long));
-	printf("Size of a float%lubytes\n", sizeof(float));
+	printf("Size of a char:%lu bytes(s)\n", sizeof(char));
+	printf("Size of an int:%lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int:%lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int:%lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float%lu bytes(s)\n", sizeof(float));
 	return (0);
 }
