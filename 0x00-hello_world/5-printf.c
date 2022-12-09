7068b5e445899feae9d38643c302531c985bae4f
Databaseadmin1 <joniasmarcos0@gmail.com>
2022-12-08 16:11:21 -0800

    separatedrules

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main - Prints "with proper grammar, but the outcome is a piece of art,"
+ * main - Prints "with proper grammar,\n"
+ * main - Prints "but the outcome is a piece of art,\n"
  *
  * Return: zero
  */
 int main(void)
 {
-	printf("with proper grammar, ");
-	printf("  but the outcome is a piece of art,");
+	printf("with proper grammar,\n");
+	printf("but the outcome is a piece of art,\n");
 	return (0);
 }
