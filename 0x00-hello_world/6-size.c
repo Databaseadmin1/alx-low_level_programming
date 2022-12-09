998a9ad5be03e0964f8bccb5463d0092b25a07bb
Databaseadmin1 <joniasmarcos0@gmail.com>
2022-12-08 17:19:17 -0800

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+{
+	printf("size of a char: %lu bytes\n", sizeof(char));
+	printf("size of an int: %lu bytes\n", sizeof(int));
+	printf("size of a long int: %lu bytes\n", sizeof(long));
+	printf("size of a lon long int:%lu bytes\n", sizeof(long long));
+	printf("size of a float:%lu bytes\n", sizeof(float));
+	return (0);
+}
