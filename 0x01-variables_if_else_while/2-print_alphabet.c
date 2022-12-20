e43f26bf2e501e1377515a8e699cdc4c2ceaddf4
Databaseadmin1 <joniasmarcos0@gmail.com>
2022-12-20 03:54:02 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Prints "alphabetic in lower case"
+ *Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	char = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch)
+		++ch
+	}
+	return (0);
+}
