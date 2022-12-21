412e808d999402d6d01eab75fbb83f6db8f02771
Databaseadmin1 <joniasmarcos0@gmail.com>
2022-12-21 10:56:11 -0800

    alphabetic menus e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints "alphabetic in lowercase except q an e"
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		ch++;
+		if ((ch != 'e') && (ch != 'q'))
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
