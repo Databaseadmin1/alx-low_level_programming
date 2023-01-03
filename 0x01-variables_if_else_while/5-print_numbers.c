9cec443ac14f3f654aaa247caf1809bf081da9c7
Databaseadmin1 <joniasmarcos0@gmail.com>
2023-01-03 14:09:48 -0800

    base 10 num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints "single digit numbers of base 10 starting from 0"
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	if (i < 10)
+	printf("%d", i);
+	++i
+	;
+	printf("\n");
+	return (0);
+}
