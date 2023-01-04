4449eb05eae605ad02d7fbbc93d30414ebf46200
Databaseadmin1 <joniasmarcos0@gmail.com>
2023-01-03 16:09:44 -0800

    putcharing ints

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -Prints "single digit numbers of base 10"
+ * Return: 0
+ */
+int main(void)
+{
+	int ch = '0';
+
+	while (ch <= 9)
+		putchar(ch);
+		++ch
+		;
+	putchar('\n');
+	return (0);
+}
