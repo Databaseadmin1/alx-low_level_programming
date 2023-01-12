e5676b5e056c61686ba872e07ed9a8bcaa4237e4
Databaseadmin1 <joniasmarcos0@gmail.com>
2023-01-12 09:08:17 -0800

    removing trailing spaces

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * print_sign - prints sign of a number
- * 
  * @n: variable to be checked
  * Return: 1 if n positive, 0 if n = zero, -1 if n <0
  */
@@ -14,7 +13,7 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar(0%10);
+		_putchar('0');
 		return (0);
 	}
 	_putchar('-');
