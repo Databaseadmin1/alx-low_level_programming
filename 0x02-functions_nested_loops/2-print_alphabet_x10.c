04cff3dc096582f0a22a52b49bf9d9c4949099d7
Databaseadmin1 <joniasmarcos0@gmail.com>
2023-01-11 23:05:24 -0800

    alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * void print_alphabet_x10(void) - Prints 10x alphabet
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int num = 1;
+
+	while (num++ <= 10)
+		for (ch = 'a'; ch <= 'z'; ch ++)
+			_putchar(ch);
+	_putchar('\n');
+}
