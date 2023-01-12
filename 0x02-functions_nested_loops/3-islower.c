6cff2bd5ebe51105a6a93628b5447c62273250e9
Databaseadmin1 <joniasmarcos0@gmail.com>
2023-01-12 04:11:05 -0800

    if it is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - check if c is lowe
+ * Return: 1 if c is lower and 0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	return (1);
+	else
+	return (0);
+}
